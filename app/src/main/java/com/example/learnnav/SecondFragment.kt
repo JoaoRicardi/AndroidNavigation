1f07f2b040b180a8a497e90f3d6e099b4f5fb419
JoaoRicardi <joao_ricardi@hotmail.com>
2020-05-01 18:26:04 -0300

    navigate to another fragment

--- app/src/main/java/com/example/learnnav/SecondFragment.kt
+++ app/src/main/java/com/example/learnnav/SecondFragment.kt
@@ -6,25 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
-import com.example.learnnav.databinding.FragmentHomeBinding
+import com.example.learnnav.databinding.SecondFragmentBinding
 
-class HomeFragment : Fragment() {
+class SecondFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val binding: FragmentHomeBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_home, container, false
+        val binding: SecondFragmentBinding = DataBindingUtil.inflate(
+            inflater, R.layout.second_fragment, container, false
         )
 
-        binding.navigateTitle.setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_navigateToFragment)
-        }
         setHasOptionsMenu(true)
         return binding.root
     }
 
     companion object {
         @JvmStatic
-        fun newInstance() = HomeFragment()
+        fun newInstance() = SecondFragment()
     }
 }
